875a959ef2ed1fad962b19eae6edb43c58d7cae8
alesem <alesemhello@gmail.com>
2022-07-23 18:36:10 +0700

    Initial commit

---
+++ Building.cs
@@ -0,0 +1,127 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BuildingPlacement
+{
+    VALID,
+    INVALID,
+    FIXED
+};
+
+public class Building
+{
+    private BuildingData _data;
+    private Transform _transform;
+    private int _currentHealth;
+
+    private BuildingPlacement _placement;
+    private List<Material> _materials;
+    private BuildingManager _buildingManager;
+
+    public Building(BuildingData data)
+    {
+        _data = data;
+        _currentHealth = data.healthpoints;
+
+        GameObject g = GameObject.Instantiate(data.prefab) as GameObject;
+        _transform = g.transform;
+
+        _buildingManager = _transform.GetComponent<BuildingManager>();
+
+        _materials = new List<Material>();
+        foreach (Material material in _transform.Find("Mesh").GetComponent<Renderer>().materials)
+        {
+            _materials.Add(new Material(material));
+        }
+
+        _placement = BuildingPlacement.VALID;
+        SetMaterials();
+    }
+
+    public void SetMaterials() { SetMaterials(_placement); }
+    public void SetMaterials(BuildingPlacement placement)
+    {
+        List<Material> materials;
+        if (placement == BuildingPlacement.VALID)
+        {
+            Material refMaterial = Resources.Load("Materials/Valid") as Material;
+            materials = new List<Material>();
+            for (int i = 0; i < _materials.Count; i++)
+            {
+                materials.Add(refMaterial);
+            }
+        }
+        else if (placement == BuildingPlacement.INVALID)
+        {
+            Material refMaterial = Resources.Load("Materials/Invalid") as Material;
+            materials = new List<Material>();
+            for (int i = 0; i < _materials.Count; i++)
+            {
+                materials.Add(refMaterial);
+            }
+        }
+        else if (placement == BuildingPlacement.FIXED)
+        {
+            materials = _materials;
+        }
+        else
+        {
+            return;
+        }
+        _transform.Find("Mesh").GetComponent<Renderer>().materials = materials.ToArray();
+    }
+
+    public void SetPosition(Vector3 position)
+    {
+        _transform.position = position;
+    }
+
+    public Transform Transform { get => _transform; }
+    public int HP { get => _currentHealth; set => _currentHealth = value; }
+    public string Code { get => _data.code; }
+    public int MaxHP { get => _data.healthpoints; }
+    public int DataIndex
+    {
+        get {
+            for (int i = 0; i < Globals.BUILDING_DATA.Length; i++)
+            {
+                if (Globals.BUILDING_DATA[i].code == _data.code)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public void Place()
+    {
+        // set placement state
+        _placement = BuildingPlacement.FIXED;
+        // change building materials
+        SetMaterials();
+        _transform.GetComponent<BoxCollider>().isTrigger = false;
+
+        // update game resources: remove the cost of the building
+        // from each game resource
+        foreach (ResourceValue resource in _data.cost)
+        {
+            Globals.GAME_RESOURCES[resource.code].AddAmount(-resource.amount);
+        }
+    }
+
+    public void CheckValidPlacement()
+    {
+        if (_placement == BuildingPlacement.FIXED) return;
+        _placement = _buildingManager.CheckPlacement()
+            ? BuildingPlacement.VALID
+            : BuildingPlacement.INVALID;
+    }
+
+    public bool IsFixed { get => _placement == BuildingPlacement.FIXED; }
+    public bool HasValidPlacement { get => _placement == BuildingPlacement.VALID; }
+    public bool CanBuy()
+    {
+        return _data.CanBuy();
+    }
+}
---
+++ UIManager.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    private BuildingPlacer _buildingPlacer;
+
+    public Transform buildingMenu;
+    public GameObject buildingButtonPrefab;
+    public Transform resourcesUIParent;
+    public GameObject gameResourceDisplayPrefab;
+
+    private Dictionary<string, TMP_Text> _resourceTexts;
+    private Dictionary<string, Button> _buildingButtons;
+
+    private void Awake()
+    {
+        // create texts for each in-game resource (gold, wood, stone...)
+        _resourceTexts = new Dictionary<string, TMP_Text>();
+        foreach (KeyValuePair<string, GameResource> pair in Globals.GAME_RESOURCES)
+        {
+            GameObject display = Instantiate(gameResourceDisplayPrefab, resourcesUIParent);
+            display.name = pair.Key;
+            _resourceTexts[pair.Key] = display.transform.Find("Text").GetComponent<TMP_Text>();
+            SetResourceText(pair.Key, (pair.Value.Amount + 99));
+        }
+
+        _buildingPlacer = GetComponent<BuildingPlacer>();
+
+        // create buttons for each building type
+        for (int i = 0; i < Globals.BUILDING_DATA.Length; i++)
+        {
+            GameObject button = GameObject.Instantiate(
+                buildingButtonPrefab,
+                buildingMenu);
+            string code = Globals.BUILDING_DATA[i].code;
+            button.name = code;
+            button.transform.Find("Text").GetComponent<Text>().text = code;
+            Button b = button.GetComponent<Button>();
+            _AddBuildingButtonListener(b, i);
+        }
+    }
+
+    private void _AddBuildingButtonListener(Button b, int i)
+    {
+        b.onClick.AddListener(() => _buildingPlacer.SelectPlacedBuilding(i));
+    }
+
+    private void SetResourceText(string resource, int value)
+    {
+        _resourceTexts[resource].text = value.ToString();
+    }
+
+    private void _OnUpdateResourceTexts()
+    {
+        foreach (KeyValuePair<string, GameResource> pair in Globals.GAME_RESOURCES)
+            SetResourceText(pair.Key, pair.Value.Amount);
+    }
+
+    private void _OnCheckBuildingButtons()
+    {
+        foreach (BuildingData data in Globals.BUILDING_DATA)
+        { 
+            _buildingButtons[data.code].interactable = data.CanBuy();
+        }
+        
+    }
+
+    private void OnEnable()
+    {
+        EventManager.AddListener("UpdateResourceTexts", _OnUpdateResourceTexts);
+        EventManager.AddListener("CheckBuildingButtons", _OnCheckBuildingButtons);
+    }
+
+    private void OnDisable()
+    {
+        EventManager.RemoveListener("UpdateResourceTexts", _OnUpdateResourceTexts);
+        EventManager.RemoveListener("CheckBuildingButtons", _OnCheckBuildingButtons);
+    }
+}
