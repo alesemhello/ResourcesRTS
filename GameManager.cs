d717f70c06fdefbac7dd9cd3729aaf0d19cf6d5c
alesem <alesemhello@gmail.com>
2022-08-30 15:40:22 +0700

    #11: Adding a day-and-night cycle

--- GameManager.cs
+++ GameManager.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public GameParameters gameParameters;
     private void Awake()
     {
         DataHandler.LoadGameData();
+        GetComponent<DayAndNightCycler>().enabled = gameParameters.enableDayAndNightCycle;
     }
 }
